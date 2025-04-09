b23cf44482596577b4371efd885db08f3e3e3995
Abhishek <abhishek.tout@gmail.com>
2025-04-09 17:19:33 +0530

    changes env variable

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,6 +1,6 @@
 export const ENV = {
     DATABASE_URL: process.env.DATABASE_URL || "postgresql://default_user:password@localhost:5432/default_db",
-    BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
+    BASE_URL: process.env.BASE_URL,
     ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET || "temp",
     REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || "temp",
     JWT_EXPIRY: process.env.JWT_EXPIRY || "1h",
