5c5aebcbe168647bc0fadcd495fd588ea911ea96
Abhishek <abhishek.tout@gmail.com>
2025-04-09 11:26:59 +0530

    upload public folder

--- src/app/employee/projects/page.jsx
+++ src/app/employee/projects/page.jsx
@@ -24,6 +24,13 @@ export default function Project() {
         }
     }
 
+    if (isLoading) {
+        return (
+            <div>Loading...</div>  
+        );
+    }
+
+
     return (
         <div className="container mx-auto px-4 rounded-xl py-3 bg-white min-h-screen">
             <h1 className="text-3xl font-bold text-gray-800 mb-5">Projects </h1>
